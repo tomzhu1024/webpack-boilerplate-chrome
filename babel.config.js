7871e27ec69c631cf39dc878c6ba709213bb85a4
Liyang Zhu <tom20000201@gmail.com>
2023-11-08 15:28:13 -0500

    fix: remove babel config

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env node */
-const env = require('./utils/env');
-
-const isDevelopment = env.NODE_ENV === 'development';
-
-module.exports = {
-  presets: ['@babel/preset-env', ['@babel/preset-react', { runtime: 'automatic' }], '@babel/preset-typescript'],
-  plugins: [isDevelopment && require.resolve('react-refresh/babel')].filter(Boolean),
-};
